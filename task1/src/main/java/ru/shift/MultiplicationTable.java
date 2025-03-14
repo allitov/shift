a50d682f5b8d6056b91caf8d340eee0c440be5f6
Alexander Litovkin <litovkinalexan@gmail.com>
2025-03-14 19:47:06 +0700

    refactor: add more readability to MultiplicationTable methods
    - Created header and body separate printers
    - Changed cell width calculation

--- task1/src/main/java/ru/shift/MultiplicationTable.java
+++ task1/src/main/java/ru/shift/MultiplicationTable.java
@@ -10,12 +10,19 @@ public class MultiplicationTable {
         String cellFormat = "|%" + cellWidth + "s";
         String rowDivider = buildRowDivider(firstCellWidth, cellWidth, size);
 
+        printHeader(size, firstCellFormat, cellFormat, rowDivider);
+        printBody(size, firstCellFormat, cellFormat, rowDivider);
+    }
+
+    private void printHeader(int size, String firstCellFormat, String cellFormat, String rowDivider) {
         System.out.printf(firstCellFormat, "");
         for (int col = 1; col <= size; col++) {
             System.out.printf(cellFormat, col);
         }
         System.out.println("\n" + rowDivider);
+    }
 
+    private void printBody(int size, String firstCellFormat, String cellFormat, String rowDivider) {
         for (int row = 1; row <= size; row++) {
             System.out.printf(firstCellFormat, row);
             for (int col = 1; col <= size; col++) {
@@ -26,13 +33,7 @@ public class MultiplicationTable {
     }
 
     private int calculateCellWidth(int maxNum) {
-        int cellWidth = 0;
-        while (maxNum != 0) {
-            maxNum = maxNum / 10;
-            cellWidth++;
-        }
-
-        return cellWidth;
+        return maxNum > 0 ? (int) Math.log10(maxNum) + 1 : 1;
     }
 
     private String buildRowDivider(int firstCellWidth, int cellWidth, int size) {
