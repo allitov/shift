f2d5cb0ffcae5c387241bb5bc24eccb440a6d603
Alexander Litovkin <litovkinalexan@gmail.com>
2025-03-14 15:17:39 +0700

    feat: add console input

--- task1/src/main/java/ru/shift/Main.java
+++ task1/src/main/java/ru/shift/Main.java
@@ -1,7 +1,23 @@
 package ru.shift;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int size = 0;
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter a size of a multiplication table (int from 1 to 32): ");
+            size = scanner.nextInt();
+            if (size < 1 || size > 32) {
+                throw new InputMismatchException();
+            }
+        } catch (InputMismatchException e) {
+            System.err.println("Illegal input format");
+            System.exit(0);
+        }
+
+        MultiplicationTable mt = new MultiplicationTable();
+        mt.printTable(size);
     }
 }
