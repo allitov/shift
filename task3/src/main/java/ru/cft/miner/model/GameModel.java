c1d16990a37b6ccbe35ee727ab6c6c406b2f2df8
Alexander Litovkin <litovkinalexan@gmail.com>
2025-04-11 17:20:28 +0700

    refactor: add game model interface

--- task3/src/main/java/ru/cft/miner/model/GameModel.java
+++ task3/src/main/java/ru/cft/miner/model/GameModel.java
@@ -1,142 +1,30 @@
 package ru.cft.miner.model;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.Random;
+public interface GameModel {
 
-public class GameModel {
+    void initGame(int rowsCount, int colsCount, int minesCount);
 
-    private Cell[][] field;
-    private int rows;
-    private int cols;
-    private int totalBombs;
+    void startGame(int startRow, int startCol);
 
-    private GameStatus status;
+    void changeFlag(int row, int col);
 
-    private FlagListener flagListener;
-    private CellRevealListener cellRevealListener;
-    private LoseListener loseListener;
+    void openCell(int row, int col);
 
-    public void initGame(int rows, int cols, int bombs) {
-        this.rows = rows;
-        this.cols = cols;
-        this.field = new Cell[rows][cols];
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                this.field[i][j] = new Cell(i, j);
-            }
-        }
-        this.totalBombs = bombs;
-        this.status = GameStatus.INIT;
-    }
+    GameStatus getStatus();
 
-    public void revealCell(int row, int col) {
-        Cell cell = this.field[row][col];
-        if (!cell.isRevealed() && !cell.isFlagged()) {
-            List<Cell> cellsToOpen = openEmptyCells(row, col);
-            if (cellRevealListener != null) {
-                cellRevealListener.onCellRevealed(cellsToOpen);
-            }
-            if (cellsToOpen.size() == 1 && cellsToOpen.get(0).isMine()) {
-                status = GameStatus.LOSE;
-                if (loseListener != null) {
-                    loseListener.onLose();
-                }
-            }
-        }
-    }
+    void registerObserver(CellObserver observer);
 
-    public void setFlag(int row, int col) {
-        Cell cell = field[row][col];
-        if (!cell.isRevealed()) {
-            cell.setFlagged(!cell.isFlagged());
-            if (flagListener != null) {
-                flagListener.onFlagSet(cell);
-            }
-        }
-    }
+    void removeObserver(CellObserver observer);
 
-    private List<Cell> openEmptyCells(int row, int col) {
-        Cell cell = field[row][col];
-        List<Cell> cellsToOpen = new LinkedList<>();
-        cell.setRevealed(true);
-        cellsToOpen.add(cell);
-        if (cell.isMine() || cell.getMinesAround() != 0) {
-            return cellsToOpen;
-        }
+    void registerObserver(FlagObserver observer);
 
-        Queue<Cell> openedCells = new LinkedList<>();
-        openedCells.add(cell);
-        while (!openedCells.isEmpty()) {
-            Cell cellToOpen = openedCells.poll();
-            for (int i = cellToOpen.getRow() - 1; i <= cellToOpen.getRow() + 1; i++) {
-                for (int j = cellToOpen.getCol() - 1; j <= cellToOpen.getCol() + 1; j++) {
-                    if (i == cellToOpen.getRow() && j == cellToOpen.getCol()) {
-                        continue;
-                    }
+    void removeObserver(FlagObserver observer);
 
-                    if (!isCellValid(i, j)) {
-                        continue;
-                    }
+    void registerObserver(WinObserver observer);
 
-                    Cell cellNeighbour = field[i][j];
-                    if (!cellNeighbour.isRevealed()) {
-                        cellNeighbour.setRevealed(true);
-                        cellsToOpen.add(cellNeighbour);
-                        if (cellNeighbour.getMinesAround() == 0) {
-                            openedCells.add(cellNeighbour);
-                        }
-                    }
-                }
-            }
-        }
+    void removeObserver(WinObserver observer);
 
-        return cellsToOpen;
-    }
+    void registerObserver(LoseObserver observer);
 
-    public void placeBombs(int x, int y) {
-        Random rand = new Random();
-        int bombsPlaced = 0;
-        while (bombsPlaced < totalBombs) {
-            int row = rand.nextInt(rows);
-            int col = rand.nextInt(cols);
-            if (!field[row][col].isMine() && !(row >= x - 1 && row <= x + 1 && col >= y - 1 && col <= y + 1)) {
-                field[row][col].setMine(true);
-                incrementCellsAround(row, col);
-                bombsPlaced++;
-            }
-        }
-        status = GameStatus.RUNNING;
-    }
-
-    private void incrementCellsAround(int row, int col) {
-        for (int i = row - 1; i <= row + 1; i++) {
-            for (int j = col - 1; j <= col + 1; j++) {
-                if (isCellValid(i, j)) {
-                    field[i][j].incrementMinesAround();
-                }
-            }
-        }
-    }
-
-    public GameStatus getStatus() {
-        return status;
-    }
-
-    private boolean isCellValid(int row, int col) {
-        return row >= 0 && row < rows && col >= 0 && col < cols && !field[row][col].isMine();
-    }
-
-    public void setFlagListener(FlagListener flagListener) {
-        this.flagListener = flagListener;
-    }
-
-    public void setCellRevealListener(CellRevealListener cellRevealListener) {
-        this.cellRevealListener = cellRevealListener;
-    }
-
-    public void setLoseListener(LoseListener loseListener) {
-        this.loseListener = loseListener;
-    }
+    void removeObserver(LoseObserver observer);
 }
