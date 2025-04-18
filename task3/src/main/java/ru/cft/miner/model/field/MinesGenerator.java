d94b537826becf731f9c6728c75c8a8d598cca4d
Alexander Litovkin <litovkinalexan@gmail.com>
2025-04-18 18:37:24 +0700

    refactor: refactor mines generator

--- task3/src/main/java/ru/cft/miner/model/field/MinesGenerator.java
+++ task3/src/main/java/ru/cft/miner/model/field/MinesGenerator.java
@@ -2,23 +2,58 @@ package ru.cft.miner.model.field;
 
 import java.util.Random;
 
+/**
+ * Класс для генерации мин на игровом поле
+ */
 public class MinesGenerator {
 
-    private final Random random = new Random();
+    private final Random random;
 
+    public MinesGenerator() {
+        random = new Random();
+    }
+
+    /**
+     * Генерирует мины на поле, исключая область вокруг начальной точки
+     * 
+     * @param field игровое поле
+     * @param minesCount количество мин для генерации
+     * @param startRow строка первого клика
+     * @param startCol столбец первого клика
+     */
     public void generateMines(GameField field, int minesCount, int startRow, int startCol) {
         int rows = field.getRows();
         int cols = field.getCols();
-        while (minesCount > 0) {
+        int remainingMines = minesCount;
+        
+        while (remainingMines > 0) {
             int row = random.nextInt(rows);
             int col = random.nextInt(cols);
-            boolean isSafeSquare = (row >= startRow - 1 && row <= startRow + 1) &&
-                    (col >= startCol - 1 && col <= startCol + 1);
-            if (!isSafeSquare && field.areCoordsValid(row, col) && !field.isCellMine(row, col)) {
+
+            if (isSafeZone(row, col, startRow, startCol)) {
+                continue;
+            }
+
+            if (field.areCoordsValid(row, col) && !field.isCellMine(row, col)) {
                 field.setCellMine(row, col, true);
-                field.changeMineCounters(row, col);
-                minesCount--;
+                updateMineCounters(field, row, col);
+                remainingMines--;
             }
         }
+    }
+
+    /**
+     * Проверяет, находится ли позиция в безопасной зоне вокруг первого клика
+     */
+    private boolean isSafeZone(int row, int col, int startRow, int startCol) {
+        return (row >= startRow - 1 && row <= startRow + 1) &&
+               (col >= startCol - 1 && col <= startCol + 1);
+    }
+
+    /**
+     * Обновляет счетчики мин вокруг указанной позиции
+     */
+    private void updateMineCounters(GameField field, int row, int col) {
+        field.changeMineCounters(row, col);
     }
 }
