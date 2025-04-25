996c9c73f35a056ece88fa2947f7010efd99a782
Alexander Litovkin <litovkinalexan@gmail.com>
2025-04-25 21:27:28 +0700

    feat: add producer-consumer system initialization

--- task5/src/main/java/ru/shift/Main.java
+++ task5/src/main/java/ru/shift/Main.java
@@ -1,6 +1,44 @@
 package ru.shift;
 
 public class Main {
+
+    private static int producerIdCounter = 0;
+    private static int consumerIdCounter = 0;
+    private static ResourceStorage storage;
+    private static AppConfig config;
+
     public static void main(String[] args) {
+        config = new AppConfig();
+        storage = new ResourceStorage(config.getStorageSize());
+        createProducers();
+        createConsumers();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        
+        System.exit(0);
+    }
+
+    private static void createProducers() {
+        for (int i = 0; i < config.getProducerCount(); i++) {
+            new Thread(new Producer(
+                    ++producerIdCounter,
+                    storage,
+                    config.getProducerTime()
+            )).start();
+        }
+    }
+
+    private static void createConsumers() {
+        for (int i = 0; i < config.getConsumerCount(); i++) {
+            new Thread(new Consumer(
+                    ++consumerIdCounter,
+                    storage,
+                    config.getConsumerTime()
+            )).start();
+        }
     }
 }
